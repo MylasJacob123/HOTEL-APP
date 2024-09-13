835d749eff2c99ea602a4a3552177e1de334fc86
nkgomotsang <nkgomotsang98@gmail.com>
2024-09-13 13:01:14 +0200

    Currently working on Rooms Page

--- hotel-app/src/components/navigation.js
+++ hotel-app/src/components/navigation.js
@@ -1,45 +1,49 @@
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./navigation.css";
 import NavLogo from "./assets/Mandala_Royal_Resort_Logo_Minimalist__5_-removebg-preview.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
 
 function Navigate() {
   return (
     <div className="navigation">
-      <div class="navbar">
+      <div className="navbar">
         <div className="logo">
           <img className="nav-Logo" src={NavLogo} alt="Logo" />
         </div>
-          <ul className="nav-items">
-            <li className="nav-list">
-              <Link to="/"><span className="nav-list-routes">Home</span></Link>
-            </li>
-            <li className="nav-list">
-              <Link to="/rooms"><span className="nav-list-routes">Rooms</span></Link>
-            </li>
-            <li className="nav-list">
-              <Link to="/facilities"><span className="nav-list-routes">Facilities</span></Link>
-            </li>
-            <li className="nav-list">
-              <Link to="/about"><span className="nav-list-routes">About</span></Link>
-            </li>
-            <li className="nav-list">
-              <Link to="/contact"><span className="nav-list-routes">Contact</span></Link>
-            </li>
+        <ul className="nav-items">
+          <li className="nav-list">
+            <NavLink to="/" exact activeClassName="active">
+              <span className="nav-list-routes">Home</span>
+            </NavLink>
+          </li>
+          <li className="nav-list">
+            <NavLink to="/rooms" activeClassName="active">
+              <span className="nav-list-routes">Rooms</span>
+            </NavLink>
+          </li>
+          <li className="nav-list">
+            <NavLink to="/facilities" activeClassName="active">
+              <span className="nav-list-routes">Facilities</span>
+            </NavLink>
+          </li>
+          <li className="nav-list">
+            <NavLink to="/about" activeClassName="active">
+              <span className="nav-list-routes">About</span>
+            </NavLink>
+          </li>
+          <li className="nav-list">
+            <NavLink to="/contact" activeClassName="active">
+              <span className="nav-list-routes">Contact</span>
+            </NavLink>
+          </li>
+        </ul>
+        <div className="profile">
+          <ul className="profile-nav-items">
+            <FontAwesomeIcon className="profile-icon" icon={faUserCircle} />
           </ul>
-          <div className="profile">
-            <ul className="profile-nav-items">
-              <FontAwesomeIcon className="profile-icon" icon={faUserCircle} />
-            {/* <li className="profile-nav-list">
-            <Link to="/register"><span className="nav-list-routes">Register</span></Link>
-            </li>
-            <li className="profile-nav-list">
-              <Link to="/login"><span className="nav-list-routes">Login</span></Link>
-            </li> */}
-          </ul>
-          </div>
+        </div>
       </div>
     </div>
   );
--- hotel-app/src/components/rooms.js
+++ hotel-app/src/components/rooms.js
@@ -1,19 +1,146 @@
-import React from 'react';
+import React from "react";
 import "./rooms.css";
 import Navigation from "./navigation";
+import FooterImg from "./assets/Rectangle 77.png";
+import RoomsFooterLogo from "./assets/Mandala Royal Resort Logo Minimalist (5).png";
 
 function Rooms() {
   return (
     <div className="rooms-display">
-        <div className="rooms-display-header">
-            <div className="rooms-display-header-top">
-                <Navigation />
-            </div>
-            <div className="rooms-display-header-bottom"></div>
+      {/* HEADER */}
+      <div className="rooms-display-header">
+        <div className="rooms-display-header-top">
+          <Navigation />
         </div>
-        <div className="rooms-display-middle"></div>
-        <div className="rooms-display-last"></div>
+        <div className="rooms-display-header-bottom">
+          <ul className="rooms-display-header-bottom-categories">
+            <div className="rooms-display-header-bottom-categories-box">
+              <li>
+                <span className="rooms-display-header-bottom-categories-box-category">
+                  All
+                </span>
+              </li>
+              <div className="rooms-display-header-bottom-categories-box-category-hover"></div>
+            </div>
+            <div className="rooms-display-header-bottom-categories-box">
+              <li>
+                <span className="rooms-display-header-bottom-categories-box-category">
+                  Single Rooms
+                </span>
+              </li>
+              <div className="rooms-display-header-bottom-categories-box-category-hover"></div>
+            </div>
+            <div className="rooms-display-header-bottom-categories-box">
+              <li>
+                <span className="rooms-display-header-bottom-categories-box-category">
+                  Double Rooms
+                </span>
+              </li>
+              <div className="rooms-display-header-bottom-categories-box-category-hover"></div>
+            </div>
+            <div className="rooms-display-header-bottom-categories-box">
+              <li>
+                <span className="rooms-display-header-bottom-categories-box-category">
+                  Queen Rooms
+                </span>
+              </li>
+              <div className="rooms-display-header-bottom-categories-box-category-hover"></div>
+            </div>
+            <div className="rooms-display-header-bottom-categories-box">
+              <li>
+                <span className="rooms-display-header-bottom-categories-box-category">
+                  King Rooms
+                </span>
+              </li>
+              <div className="rooms-display-header-bottom-categories-box-category-hover"></div>
+            </div>
+            <div className="rooms-display-header-bottom-categories-box">
+              <li>
+                <span className="rooms-display-header-bottom-categories-box-category">
+                  Presidential Suite
+                </span>
+              </li>
+              <div className="rooms-display-header-bottom-categories-box-category-hover"></div>
+            </div>
+          </ul>
+          <div className="rooms-display-header-bottom-categories-line"></div>
+        </div>
+      </div>
+
+      {/* MIDDLE */}
+      <div className="rooms-display-middle">
+        <div class="rooms-display-middle-cards">
+          <div className="room-display-card"></div>
+          <div className="room-display-card"></div>
+          <div className="room-display-card"></div>
+          <div className="room-display-card"></div>
+          <div className="room-display-card"></div>
+          <div className="room-display-card"></div>
+          <div className="room-display-card"></div>
+          <div className="room-display-card"></div>
+          <div className="room-display-card"></div>
+        </div>
+      </div>
+
+      {/* LAST */}
+      <div className="rooms-display-last">
+        <div className="rooms-display-last-top">
+          <p className="rooms-display-last-top-paragraph">
+            Elevate your stay by discovering our full array of exquisite rooms
+            and suites. Each one offers a unique blend of luxury and
+            sophistication, meticulously designed to provide the ultimate in
+            comfort and style. Click below to explore our exclusive selections
+            and find the perfect retreat tailored to your desires.
+          </p>
+          <div className="rooms-display-last-top-image-div">
+            <img
+              src={FooterImg}
+              alt="Image of a couple sharing laughter in a hotel bed"
+            />
+          </div>
+        </div>
+        <div className="rooms-display-last-middle">
+          <div className="rooms-display-last-middle-btn-navigation"></div>
+        </div>
+        <div className="rooms-display-last-bottom">
+          <div className="rooms-display-last-bottom-top">
+            <div className="rooms-display-last-bottom-top1">
+              <div className="rooms-display-last-bottom-top1-logo-div">
+                <div className="rooms-display-last-bottom-top1-logo">
+                  <img src={RoomsFooterLogo} alt="Logo of the hotel" />
+                </div>
+                <h6>20 Houston Park Ave</h6>
+                <h6>Evelyn, Richmond Monaco 2322</h6>
+              </div>
+              <div className="rooms-display-last-bottom-top1-overview">
+              <div>
+                  <h6>Rooms</h6>
+                  <h6>Facilities</h6>
+                  <h6>About</h6>
+                </div>
+                <div>
+                  <h6>Instagram</h6>
+                  <h6>Twitter</h6>
+                  <h6>YouTube</h6>
+                </div>
+              </div>
+            </div>
+            <div className="rooms-display-last-bottom-top2">
+            <h6>Contact Details</h6>
+              <p className="rooms-display-last-bottom-top2-contact-details">
+                greywood@gmail.com
+                <br />
+                +27 81 007 1498
+              </p>
+            </div>
+          </div>
+          <div className="rooms-display-last-bottom-bottom">
+            <div></div>
+            <div></div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-export default  Rooms;
+export default Rooms;
